32a0c234b19ac323bad2ce8afe20f267417515f1
atiwa115 <lakshyatiwari14@gmail.com>
2023-02-01 21:52:53 +0530

    Create hiring-manager.module.ts

---
+++ src/app/hiring-manager/hiring-manager.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HmHomepageComponent } from './hm-homepage/hm-homepage.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    HmHomepageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HiringManagerModule { }
